de534500d1538e6135f32f80b22c4107b01c7013
Abdullrahmen <abdullrahmen2003@gmail.com>
2024-12-26 03:13:08 +0200

    remove rotate animation

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,39 +2,39 @@ import NavBar from '../NavBar/NavBar';
 import './Header.css';
 
 function HeaderText() {
-    return (
-        <div className="header-text">
-            <h1>CircuitCart</h1>
-            <p>E-commerce for electronic circuit components </p>
-            <p>Get your electronic components at the best prices</p>
-            <button className="btn">Shop Now</button>
-            <div className="loader"></div>
-        </div>
-    );
+  return (
+    <div className="header-text">
+      <h1>CircuitCart</h1>
+      <p>E-commerce for electronic circuit components </p>
+      <p>Get your electronic components at the best prices</p>
+      <button className="btn">Shop Now</button>
+      {/* <div className="loader"></div> */}
+    </div>
+  );
 }
 
 function HeaderImage() {
-    return (
-        <div className="header-image">
-            <img src="src\assets\HeaderImage.png" alt="Circuit image" />
-        </div>
-    );
+  return (
+    <div className="header-image">
+      <img src="src\assets\HeaderImage.png" alt="Circuit image" />
+    </div>
+  );
 }
 
 function Header() {
-    return (
-        <>
-            <NavBar />
-            <div className="header">
-                <div className="header-container">
-                    <HeaderText />
-                    <HeaderImage />
-                </div>
-                {/*Todo <svg wave> https://svgwave.in/
+  return (
+    <>
+      <NavBar />
+      <div className="header">
+        <div className="header-container">
+          <HeaderText />
+          <HeaderImage />
+        </div>
+        {/*Todo <svg wave> https://svgwave.in/
 				blog.logrocket.com/create-wavy-background-using-css-svg/*/}
-            </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Header;
